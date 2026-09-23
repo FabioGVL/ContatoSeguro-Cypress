2e77403faa615a1ac9ce480048137d41f90e99dd
FabioGVL <135769523+FabioGVL@users.noreply.github.com>
2026-09-23 13:35:38 -0300

    Contato Seguro Automation

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
-    "chromeWebSecurity": false,
-    "baseUrl": "http://localhost:5400"
+
+    chromeWebSecurity: false,
+    baseUrl: "http://localhost:5400"
   },
-  video: true 
+
+  video: true,
+
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Contato Seguro - Relatório de Testes",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false
+  }
 });
